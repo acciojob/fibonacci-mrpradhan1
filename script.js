e207f295f48dcd8aae7f521ca96925a9aaac1d3c
Manoranjan Pradhan <133505638+mrpradhan1@users.noreply.github.com>
2023-07-18 23:55:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
 // your code here
+	let a=0;
+	let b=1;
+	if(num<=2)
+		return n-1;
+	for(let i=0;i<n-2;i++){
+		let temp = b;
+		b = a + b ;
+		a=temp;
+	}
+	return b;
 }
+console.log(fibonacci);
 
 module.exports = fibonacci;
